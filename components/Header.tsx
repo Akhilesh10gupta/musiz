1e4ccf72033fd025dbfd1065a424e0e74abd7e6d
guptaji <akhilshorts87@gmail.com>
2025-06-06 10:35:00 +0530

    Layout added for musiz

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from './Container'
+
+const Header = () => {
+  return (
+    <header>
+      <Container className=''>
+        header
+      </Container>
+    </header>
+  )
+}
+
+export default Header

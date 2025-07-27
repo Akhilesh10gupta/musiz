165a541a204e5704909fd5631e35c57bf44bff20
guptaji <akhilshorts87@gmail.com>
2025-07-28 00:44:10 +0530

    Loading Screen added

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,53 @@
-import React from 'react'
+'use client'
+
+import React, { useState, useEffect } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
 import Container from '@/components/Container'
 import Hero from '@/components/Hero'
 import Services from '@/components/Services'
 import Projects from '@/components/Projects'
 import About from '@/components/aboutus'
-
+import LoadingScreen from '@/components/LoadingScreen'
 
 const Home = () => {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 2000) // You can adjust this duration
+
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
-    <Container className='bg-gradient-to-br from-teal-100 via-cyan-50 to-blue-50 max-w-full' >
-      
-      <Hero/>
-      <Projects/>
-      <Services />
-      <About/>
-    </Container>
+    <>
+      <AnimatePresence>
+        {loading && (
+          <motion.div
+            key="loader"
+            exit={{ opacity: 0, transition: { duration: 0.5 } }}
+          >
+            <LoadingScreen />
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+      {!loading && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8 }}
+        >
+          <Container className="bg-gradient-to-br from-teal-100 via-cyan-50 to-blue-50 max-w-full">
+            <Hero />
+            <Projects />
+            <Services />
+            <About />
+          </Container>
+        </motion.div>
+      )}
+    </>
   )
 }
 
